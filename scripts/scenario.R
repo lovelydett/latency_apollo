607b2270c3c1ef918f93e8861b3bca13db543327
lovelydett <xyt@bupt.cn>
2022-07-07 03:57:56 +0800

    [New] scenario result analysis

--- scripts/scenario.R
+++ scripts/scenario.R
@@ -23,6 +23,7 @@ library("assert")
 library("gridExtra")
 library("cowplot")
 library("ggstatsplot")
+library("tidyr")
 
 # Turn E-Epress off
 options(scipen = 999)
@@ -42,19 +43,30 @@ white_noise_test <- function(df) {
     # plot(ts_et)
 }
 
-driving_info_correlation <- function(df) {
-    g <- ggplot(df, aes(x = id))
-    g <- g + geom_line(aes(y = execution_time)) # execution time
-    g <- g + geom_line(aes(y = info * 10)) # info
+driving_info_correlation <- function(df, coeff = 10, info_name = "info") {
+    # Align unit for info
+    df$info <- df$info * coeff
+
+    # Label row
+    df <- pivot_longer(
+        df,
+        cols = c("info", "execution_time"),
+        names_to = "line_type",
+        values_to = "line_value"
+    )
+    g <- ggplot(df, aes(x = id, y = line_value, colour = line_type))
+    g <- g + geom_line()
+    g <- g + geom_point()
     g <- g + scale_y_continuous(
         name = "Execution time (ms)",
-        sec.axis = sec_axis(~./10, name = "Scenario Info")
+        sec.axis = sec_axis(~. / coeff, name = "Scenario Info")
     )
-    g <- g + scale_colour_Publication() + theme_Publication()
-    my_plot(g, name = "Scenario2")
+    g <- g + scale_color_hue(labels = c("execution_time" = "Execution time", "info" = info_name))
+    g <- g + theme_Publication() + theme(legend.title = element_blank())
+    my_plot(g, name = "Scenario")
 }
 
-df <- load_data("../data/dataset2_driving_info/1/planning.csv", finish_only = TRUE, round = FALSE, is_smooth = FALSE)
+df <- load_data("../data/dataset2_driving_info/2/planning.csv", finish_only = TRUE, round = TRUE, is_smooth = FALSE)
 driving_info_correlation(df)
 
 dev.off()
