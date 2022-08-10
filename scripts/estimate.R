0c0dc49c39cde7db46d9dcc4c13335fdcb4d17f6
lovelydett <xyt@bupt.cn>
2022-08-10 20:00:20 +0800

    [New] pred

--- scripts/estimate.R
+++ scripts/estimate.R
@@ -8,8 +8,8 @@
 # 3. Determine d, p, q in ARIMA(d, p, q).
 # 4. Plot graph
 
-setwd("/Users/yuting/Codes/latency_apollo/scripts")
-# setwd("/home/tt/Codes/latency_apollo/scripts")
+# setwd("/Users/yuting/Codes/latency_apollo/scripts")
+setwd("/home/tt/Codes/latency_apollo/scripts")
 # setwd("D:\\Codes\\latency_apollo\\scripts")
 
 # Tools
@@ -27,12 +27,13 @@ library("ggstatsplot")
 library("tidyr")
 library("tseries")
 library("forecast")
+library("pracma")
 
 # Turn E-Epress off
 options(scipen = 999)
 
 # Get the ACF and PACF graph respectively for a specific df
-get_acf_pacf <- function(df, task_name, color = "#c554b6") {
+get_acf_pacf <- function(df, task_name) {
     ts_et <- ts(df$execution_time, start = 1)
     # test_result <- Box.test(ts_et, lag = 1, fitdf = 0)
     ts_et.acf <- acf(ts_et, plot = FALSE)
@@ -61,6 +62,35 @@ build_arima <- function(df, task, p, d, q) {
     return(list(id = c(1 : length(df_ts)), predicted = predicted, truth = df_ts, residuals = as.double(model$residuals))) # Test the model
 }
 
+plot_acf_pacf <- function(df_acf, df_pacf, nused_acf, nused_pacf, task_name, color = "#c554b6") {
+    lim1 <- qnorm((1 + (1 - 0.05)) / 2) / sqrt(nused_acf)
+    lim0 <- -1 * lim1
+    g_acf <- ggplot(df_acf, mapping = aes(x = lag, y = acf)) +
+        geom_bar(stat = "identity", fill = color) +
+        geom_hline(aes(yintercept = 0), size = 1.5) +
+        # geom_segment(mapping = aes(xend = lag, yend = 0)) +
+        geom_hline(aes(yintercept = lim1), linetype = 2, colour = "#d50025", size = 1.3) +
+        geom_hline(aes(yintercept = lim0), linetype = 2, colour = "#d50025", size = 1.3) +
+        coord_cartesian(xlim = c(0, 20)) +
+        labs(x = "Lag", y = "ACF") +
+        scale_fill_Publication() +
+        theme_Publication()
+    lim1 <- qnorm((1 + (1 - 0.05)) / 2) / sqrt(nused_pacf)
+    lim0 <- -1 * lim1
+    g_pacf <- ggplot(df_pacf, mapping = aes(x = lag, y = acf)) +
+        geom_bar(stat = "identity", fill = color) +
+        geom_hline(aes(yintercept = 0), size = 1.5) +
+        # geom_segment(mapping = aes(xend = lag, yend = 0)) +
+        geom_hline(aes(yintercept = lim1), linetype = 2, colour = "#d50025", size = 1.3) +
+        geom_hline(aes(yintercept = lim0), linetype = 2, colour = "#d50025", size = 1.3) +
+        coord_cartesian(xlim = c(0, 20)) +
+        labs(x = "Lag", y = "PACF") +
+        scale_fill_Publication() +
+        theme_Publication()
+    g <- grid.arrange(g_acf, g_pacf, ncol = 2, nrow = 1)
+    my_plot(g, name = paste0("ACF_PACF_", task_name), height = 3, width = 10)
+}
+
 fit_arima_example <- function() {
     dataset_dir <- "../data/dataset2_driving_info/1/"
     task <- "prediction"
@@ -74,12 +104,12 @@ fit_arima_example <- function() {
     res$predicted[res$predicted < 0] <- 10
 
     # 1. Plot correlation of predicted and truth values.
-    g_correlation <- ggplot(res, mapping = aes(x = predicted, y = truth)) +
-        geom_point() +
-        geom_abline(slope = 1, intercept = 0, colour = "#8E0C24", size = 2) +
-        coord_cartesian(xlim = c(0, 75), ylim = c(0, 75)) +
-        scale_colour_Publication() +
-        theme_Publication()
+    # g_correlation <- ggplot(res, mapping = aes(x = predicted, y = truth)) +
+    #     geom_point() +
+    #     geom_abline(slope = 1, intercept = 0, colour = "#8E0C24", size = 2) +
+    #     coord_cartesian(xlim = c(0, 75), ylim = c(0, 75)) +
+    #     scale_colour_Publication() +
+    #     theme_Publication()
     # my_plot(g, name = paste0("R_predicted_truth_", task), height = 10, width = 10)
     # print(graphs[1])
     # g <- grid.arrange(graphs[[1]], graphs[[2]], graphs[[3]], graphs[[4]], ncol = 2, nrow = 2)
@@ -96,27 +126,29 @@ fit_arima_example <- function() {
     lim1 <- qnorm((1 + (1 - 0.05)) / 2) / sqrt(nused_acf)
     lim0 <- -1 * lim1
     g_acf <- ggplot(df_acf, mapping = aes(x = lag, y = acf)) +
-        geom_bar(stat = "identity", fill = "#c554b6") +
+        geom_bar(stat = "identity", fill = "#2470a0") +
         geom_hline(aes(yintercept = 0), size = 1.5) +
         # geom_segment(mapping = aes(xend = lag, yend = 0)) +
-        geom_hline(aes(yintercept = lim1), linetype = 2, colour = "#FFD800", size = 1.3) +
-        geom_hline(aes(yintercept = lim0), linetype = 2, colour = "#FFD800", size = 1.3) +
+        geom_hline(aes(yintercept = lim1), linetype = 2, colour = "#d50025", size = 1.3) +
+        geom_hline(aes(yintercept = lim0), linetype = 2, colour = "#d50025", size = 1.3) +
         coord_cartesian(xlim = c(0, 20)) +
-        labs(x = "Lag", y = "(P)ACF") +
+        labs(x = "Lag", y = "ACF") +
         scale_fill_Publication() +
-        theme_Publication()
+        theme_Publication() +
+        theme(axis.title = element_text(face = "bold",size = rel(0.6)), axis.text = element_text(size = rel(0.5)))
     lim1 <- qnorm((1 + (1 - 0.05)) / 2) / sqrt(nused_pacf)
     lim0 <- -1 * lim1
     g_pacf <- ggplot(df_pacf, mapping = aes(x = lag, y = acf)) +
-        geom_bar(stat = "identity", fill = "#c554b6") +
+        geom_bar(stat = "identity", fill = "#2470a0") +
         geom_hline(aes(yintercept = 0), size = 1.5) +
         # geom_segment(mapping = aes(xend = lag, yend = 0)) +
-        geom_hline(aes(yintercept = lim1), linetype = 2, colour = "#FFD800", size = 1.3) +
-        geom_hline(aes(yintercept = lim0), linetype = 2, colour = "#FFD800", size = 1.3) +
+        geom_hline(aes(yintercept = lim1), linetype = 2, colour = "#d50025", size = 1.3) +
+        geom_hline(aes(yintercept = lim0), linetype = 2, colour = "#d50025", size = 1.3) +
         coord_cartesian(xlim = c(0, 20)) +
         labs(x = "Lag", y = "PACF") +
         scale_fill_Publication() +
-        theme_Publication()
+        theme_Publication() +
+        theme(axis.title = element_text(face = "bold",size = rel(0.6)), axis.text = element_text(size = rel(0.5)))
     # g_2 <- grid.arrange(g_acf, g_pacf, ncol = 2, nrow = 1)
     # my_plot(g, name = paste0("ACF_PACF_", "prediction_residuals"), height = 3, width = 10)
 
@@ -147,20 +179,54 @@ fit_arima_example <- function() {
         theme(legend.position = c(0.5, 0.7), legend.title = element_blank())
     # my_plot(g, name = paste0("Truth_Predicted_", task), height = 2, width = 6)
 
+
+    # Hacker: use moving average
+    df <- df[300:499,]
+    df$id <- df$id - 300
+    df$et_pred <- movavg(df$execution_time, 2, "s")
+    for (i in 4 : nrow(df)) {
+        df[i, "et_pred"] <- 0.5 * df[i - 1, "execution_time"] + 0.3 * df[i - 2, "execution_time"] + 0.2 * df[i - 3, "execution_time"]
+    }
+    print(paste0("Pearson: ", cor(df$et_pred, df$execution_time, method = "pearson")))
+
+    df_gt <- df %>% select(id, execution_time)
+    df_pred <- df %>% select(id, et_pred)
+    df_gt$type <- "ground truth"
+    df_pred$type <- "predicted"
+    names(df_gt) <- c("id", "value", "type")
+    names(df_pred) <- c("id", "value", "type")
+
+    print(head(df_gt))
+
+    g_correlation <- ggplot(df, mapping = aes(x = et_pred, y = execution_time)) +
+        geom_point() +
+        geom_abline(slope = 1, intercept = 0, colour = "#8E0C24", size = 2) +
+        coord_cartesian(xlim = c(0, 60), ylim = c(0, 50)) +
+        labs(x = "Predicted (ms)", y = "Ground truth (ms)") +
+        scale_colour_Publication() +
+        theme_Publication() +
+        theme(axis.title = element_text(face = "bold", size = rel(0.6)), axis.text = element_text(size = rel(0.5)))
+    
+    df_to_plot <- rbind(df_gt, df_pred)
+    g_series <- ggplot(df_to_plot, mapping = aes(x = id, y = value, color = type)) +
+        geom_line() +
+        geom_point() +
+        labs(x = "Input message sequence", y = "Execution time (ms)") +
+        theme_Publication() + scale_colour_Publication() +
+        theme(legend.position = c(0.2, 0.8), legend.title = element_blank(), legend.direction = "vertical", axis.title = element_text(face = "bold",size = rel(0.6)), axis.text = element_text(size = rel(0.5)))
+
     # Arrange these figures in one figure.
     g <- grid.arrange(g_series, arrangeGrob(g_correlation, g_acf, g_pacf, ncol = 3))
-    my_plot(g, name = paste0("ARIMA_Result_", task), height = 4, width = 8)
-    
+    my_plot(g, name = paste0("Pred_Result_", task), height = 4, width = 8)
 }
 
 acf_pacf <- function(dataset_dir) {
     i <- 1
     for (task in TASKS) {
         df <- load_data(paste0(dataset_dir, task, ".csv"), finish_only = FALSE, round = TRUE, is_smooth = FALSE)
-        ret <- get_acf_pacf(df, task, color = MY_COLORS[i])
+        ret <- get_acf_pacf(df, task)
         # determine_dpq_arima(df, task)
-        # df_acf <- ret[1]
-        # df_pacf <- ret[2]
+        plot_acf_pacf(ret[[1]], ret[[2]], ret[[3]], ret[[4]], task_name = task, color = MY_COLORS_DEEP[i])
         i <- i + 1
     }
 }
--- scripts/modeling.R
+++ scripts/modeling.R
@@ -10,8 +10,8 @@
 # 5. For each edge, judge the predicability.
 # 6. Incorporate the sensor freq to complete the prediction model.
 
-setwd("/Users/yuting/Codes/latency_apollo/scripts")
-# setwd("/home/tt/Codes/latency_apollo/scripts")
+# setwd("/Users/yuting/Codes/latency_apollo/scripts")
+setwd("/home/tt/Codes/latency_apollo/scripts")
 # setwd("D:\\Codes\\latency_apollo\\scripts")
 
 # Tools
@@ -28,6 +28,8 @@ library("cowplot")
 library("ggstatsplot")
 library("tidyr")
 library("tseries")
+library("ggpubr")
+library("pracma")
 
 # Turn E-Epress off
 options(scipen = 999)
@@ -39,7 +41,7 @@ control_exit_latency <- function(filename) {
     df_whole$lat_lidar <- df_whole$lat_lidar / MS_TO_NS
     df_whole$lat_radar <- df_whole$lat_radar / MS_TO_NS
 
-    # From the view of control command information delay
+    # From the view of IVP
     df <- df_whole[2000:2200, ]
     df_cam <- df %>% select(ts_cam, lat_cam)
     df_lidar <- df %>% select(ts_lidar, lat_lidar)
@@ -55,14 +57,14 @@ control_exit_latency <- function(filename) {
     df_radar$sensor <- "Radar"
     df <- rbind(df_cam, df_lidar, df_radar)
     g_lat <- ggplot(df, aes(x = id, y = lat, colour = sensor)) +
-        geom_line() + geom_point() +
-        labs(title = "Information Vacuum Period for Control Command", x = "Control Command Sequence", y = "Period (ms)") +
+        geom_line(size = 1.5) + geom_point(size = 3) +
+        labs(title = "Information vacuum period in ControlCommand", x = "ControlCommand sequence", y = "Period length (ms)") +
         coord_cartesian(ylim = c(1, 300)) +
         scale_colour_Publication() +  theme_Publication() + 
         theme(legend.title = element_blank())
-    my_plot(g_lat, "IVP")
+    # my_plot(g_lat, "IVP")
 
-    # From the view of information processing time
+    # From the view of EIL
     df <- df_whole
     df_cam <- df %>% select(ts_cam, lat_cam)
     df_lidar <- df %>% select(ts_lidar, lat_lidar)
@@ -73,7 +75,7 @@ control_exit_latency <- function(filename) {
     df_cam$sensor <- "Camera"
     df_lidar$sensor <- "LiDAR"
     df_radar$sensor <- "Radar"
-    df_cam <- df_cam[!duplicated(df_cam$ts), ]
+    df_cam <- df_cam[!duplicated(df_cam$ts), ] # The first latency record for each timestamp is its E2E latency.
     df_lidar <- df_lidar[!duplicated(df_lidar$ts), ]
     df_radar <- df_radar[!duplicated(df_radar$ts), ]
     start <- 400
@@ -85,18 +87,33 @@ control_exit_latency <- function(filename) {
     df_lidar$id <- c(1:len)
     df_radar$id <- c(1:len)
     df <- rbind(df_cam, df_lidar, df_radar)
-    print(df[1:10,])
     g_process <- ggplot(df, aes(x = id, y = lat, color = sensor)) +
-        geom_line() + geom_point() +
-        labs(title = "E2E Processing Time", x = "Raw sensor data sequence", y = "Latency (ms)") +
+        geom_line(size = 1.5) + geom_point(size = 3) +
+        labs(title = "End-to-end processing time", x = "Sensory input sequence", y = "Latency (ms)") +
         coord_cartesian(ylim = c(1, 300)) +
-        scale_colour_Publication() + theme_Publication() + 
+        scale_colour_Publication() + theme_Publication() +
         theme(legend.title = element_blank())
-    my_plot(g_process, "E2E_Process")
+    # my_plot(g_process, "E2E_Process")
 
     # Plot g_lat and g_process in one graph
-    g <- grid.arrange(g_lat, g_process, ncol = 2, nrow = 1)
+    if (0) {
+        g <- ggarrange(g_lat, g_process, ncol = 2, nrow = 1, common.legend = TRUE, legend = "bottom")
         my_plot(g, "E2E", width = 20, height = 5)
+    }
+
+    ### Do moving average directly on EIL, dont use!
+    df_cam <- df_whole %>% select(ts_cam, lat_cam)
+    df_lidar <- df_whole %>% select(ts_lidar, lat_lidar)
+    df_radar <- df_whole %>% select(ts_radar, lat_radar)
+    df_cam <- df_cam[!duplicated(df_cam$ts), ] # The first latency record for each timestamp is its E2E latency.
+    df_lidar <- df_lidar[!duplicated(df_lidar$ts), ]
+    df_radar <- df_radar[!duplicated(df_radar$ts), ]
+    
+    df_cam$lat_cam_pred <- movavg(df_cam$lat_cam, 3, "s")
+    # print(head(df_cam))
+
+    # plot(df_cam$lat_cam, df_cam$lat_cam_pred)
+    cor(df_cam$lat_cam, df_cam$lat_cam_pred, method = "pearson")
 }
 
 graph_builder <- function(channels) {
