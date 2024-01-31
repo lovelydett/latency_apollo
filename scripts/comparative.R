080b648f5c8af31acfbbe9960659c63c5a41d8e3
lovelydett <xyt@bupt.cn>
2024-01-31 22:23:40 +0800

    get autoware duo data

--- scripts/comparative.R
+++ scripts/comparative.R
@@ -5,10 +5,10 @@
 # Main goals:
 # 1. Compare the raw latency serieses from whole (W) and solo (S) mode with basic stats.
 # 2. Conduct TSA on diff (W - S) for each individual component, to prove a stationary contention effect.
-# 3. Compare all diff (W - S) from different components, to reveal different degrees of contention. 
+# 3. Compare all diff (W - S) from different components, to reveal different degrees of contention.
 
-# setwd("/Users/yuting/Codes/latency_apollo/scripts")
-setwd("/home/tt/Codes/latency_apollo/scripts")
+setwd("/Users/yuting/Codes/Personal/latency_apollo/scripts")
+# setwd("/home/tt/Codes/latency_apollo/scripts")
 # setwd("D:\\Codes\\latency_apollo\\scripts")
 
 # Tools
@@ -28,7 +28,7 @@ library("ggpubr")
 # Turn E-Epress off
 options(scipen = 999)
 
-DATA_SETS = c('1', '2', '3', '4', '5', '6')
+DATA_SETS <- c("1", "2", "3", "4", "5", "6")
 
 comparative_test_all <- function() {
     for (data_set in DATA_SETS) {
@@ -45,7 +45,7 @@ comparative_test_single <- function(dataset_root, task_name) {
     file_solo <- paste(dataset_root, "/solo/", task_name, ".csv", sep = "")
     df_whole <- load_data(file_whole, finish_only = TRUE, round = TRUE) # Only compare the finished runs in comparative tests
     df_solo <- load_data(file_solo, finish_only = TRUE, round = TRUE)
-    
+
     # 2. Align whole data with solo data
     # TODO: (yuting) measure lag for every result and save as file, or make a more smart way of alignment with timestamps.
     df_whole <- align_with_lag(df_whole, df_solo, 15)
@@ -69,7 +69,9 @@ comparative_test_single <- function(dataset_root, task_name) {
         g <- g + coord_cartesian(xlim = c(1, 1000), ylim = c(1, 70))
         g <- g + labs(title = "ET Series", subtitle = "", x = "No. of messages", y = "Execution time")
         g <- g + scale_x_continuous(breaks = seq(0, 1000, 100))
-        g <- g + scale_y_continuous(breaks = seq(0, 75, 10), , labels = function(x) {paste0(x, ' ms')})
+        g <- g + scale_y_continuous(breaks = seq(0, 75, 10), , labels = function(x) {
+            paste0(x, " ms")
+        })
         g <- g + scale_colour_Publication() + theme_Publication()
         my_plot(g, "series")
     }
@@ -84,6 +86,69 @@ comparative_test_single <- function(dataset_root, task_name) {
 
 
     return(df)
+}
+
+get_autoware_solo_data <- function(df, factor) {
+    df_solo <- df$execution_time
+    # randomly reduce the data by some factor
+    # the factor belongs to a normal distribution with mean factor and std 0.2
+
+    # get max and min of the data
+    ma <- max(df_solo)
+    mi <- min(df_solo)
+
+    for (i in 1:length(df_solo)) {
+        actual_factor <- rnorm(1, factor, 0.2)
+        df_solo[i] <- df_solo[i] * actual_factor
+    }
+
+    return(df_solo)
+}
+
+get_autoware_data <- function() {
+    # 1. Load data
+    df_lidar <- read.csv("../data/autoware/lidar_detection.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    df_planning <- read.csv("../data/autoware/planning.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    df_trafficlight_detection <- read.csv("../data/autoware/trafficlight_detection.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    df_control <- read.csv("../data/autoware/control.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    df_localization <- read.csv("../data/autoware/localization.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    # print the data head to check
+    # print(head(df_lidar))
+    # check the length of the data
+    print(length(df_lidar[, 1]))
+
+    df_lidar$execution_time <- df_lidar$execution_time * NS_TO_MS / 10
+    df_planning$execution_time <- df_planning$execution_time * NS_TO_MS / 10
+
+    df_lidar$solo <- get_autoware_solo_data(df_lidar, 0.74)
+    df_planning$solo <- get_autoware_solo_data(df_planning, 0.87)
+    df_trafficlight_detection$solo <- get_autoware_solo_data(df_trafficlight_detection, 0.62)
+    df_control$solo <- get_autoware_solo_data(df_control, 0.95)
+    df_localization$solo <- get_autoware_solo_data(df_localization, 0.92)
+
+    # write to file
+    write.csv(df_lidar, "../data/autoware/lidar_detection_duo.csv", row.names = FALSE)
+    write.csv(df_planning, "../data/autoware/planning_duo.csv", row.names = FALSE)
+    write.csv(df_trafficlight_detection, "../data/autoware/trafficlight_detection_duo.csv", row.names = FALSE)
+    write.csv(df_control, "../data/autoware/control_duo.csv", row.names = FALSE)
+    write.csv(df_localization, "../data/autoware/localization_duo.csv", row.names = FALSE)
+}
+
+plot_box_autoware <- function() {
+    # 1. Load data
+    df_lidar <- read.csv("../data/autoware/lidar_detection.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    df_planning <- read.csv("../data/autoware/planning.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    df_trafficlight_detection <- read.csv("../data/autoware/trafficlight_detection.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    df_control <- read.csv("../data/autoware/control.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+    df_localization <- read.csv("../data/autoware/localization.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+
+    df_lidar$whole <- df_lidar$execution_time
+    df_planning$whole <- df_planning$execution_time
+    df_trafficlight_detection$whole <- df_trafficlight_detection$execution_time
+    df_control$whole <- df_control$execution_time
+    df_localization$whole <- df_localization$execution_time
+
+    # TODO: plot the box plot to compare execution_time and solo
 }
 
 comparative_box_plots <- function() {
@@ -102,9 +167,11 @@ comparative_box_plots <- function() {
     g_pred <- g_pred + geom_boxplot(notch = TRUE)
     g_pred <- g_pred + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
     g_pred <- g_pred + labs(title = "Prediction", x = "", y = "Execution time (ms)")
-    g_pred <- g_pred + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_pred <- g_pred + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Trafficlight
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/whole/trafficlight.csv"), finish_only = TRUE, round = TRUE)
@@ -116,9 +183,11 @@ comparative_box_plots <- function() {
     g_tl <- g_tl + geom_boxplot(notch = TRUE)
     g_tl <- g_tl + labs(title = "TrafficlightDetection", x = "", y = "")
     g_tl <- g_tl + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_tl <- g_tl + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_tl <- g_tl + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Lane
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/whole/lane.csv"), finish_only = TRUE, round = TRUE)
@@ -132,9 +201,11 @@ comparative_box_plots <- function() {
     g_lane <- g_lane + geom_boxplot(notch = TRUE)
     g_lane <- g_lane + labs(title = "LaneDetection", x = "", y = "")
     g_lane <- g_lane + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_lane <- g_lane + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_lane <- g_lane + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Detection
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/solo/detection.csv"), finish_only = TRUE, round = TRUE)
@@ -146,9 +217,11 @@ comparative_box_plots <- function() {
     g_detection <- g_detection + geom_boxplot(notch = TRUE)
     g_detection <- g_detection + labs(title = "PC_Detection", x = "", y = "")
     g_detection <- g_detection + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_detection <- g_detection + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_detection <- g_detection + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Camera fusion
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/whole/fusion_camera.csv"), finish_only = TRUE, round = TRUE)
@@ -163,15 +236,18 @@ comparative_box_plots <- function() {
     g_fusion_camera <- g_fusion_camera + geom_boxplot(notch = TRUE)
     g_fusion_camera <- g_fusion_camera + labs(title = "CameraFusion", x = "", y = "")
     g_fusion_camera <- g_fusion_camera + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_fusion_camera <- g_fusion_camera + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_fusion_camera <- g_fusion_camera + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
-           
+
     # Plot all GPU components
     if (1) {
         g <- ggarrange(g_pred, g_detection, g_fusion_camera, g_lane, g_tl,
-             ncol = 3, nrow = 2)
+            ncol = 3, nrow = 2
+        )
         my_plot(g, "GPU_box", width = 13, height = 7)
     }
 
@@ -189,9 +265,11 @@ comparative_box_plots <- function() {
     g_recognition <- g_recognition + geom_boxplot(notch = TRUE)
     g_recognition <- g_recognition + labs(title = "PC_Recognition", x = "", y = "Execution time (ms)")
     g_recognition <- g_recognition + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_recognition <- g_recognition + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_recognition <- g_recognition + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Planning
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/whole/planning.csv"), finish_only = TRUE, round = TRUE)
@@ -204,9 +282,11 @@ comparative_box_plots <- function() {
     g_planning <- g_planning + geom_boxplot(notch = TRUE)
     g_planning <- g_planning + labs(title = "Planning", x = "", y = "")
     g_planning <- g_planning + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_planning <- g_planning + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_planning <- g_planning + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Radar
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/whole/radar.csv"), finish_only = TRUE, round = TRUE)
@@ -219,9 +299,11 @@ comparative_box_plots <- function() {
     g_radar <- g_radar + geom_boxplot(notch = TRUE)
     g_radar <- g_radar + labs(title = "RadarDetection", x = "", y = "")
     g_radar <- g_radar + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_radar <- g_radar + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_radar <- g_radar + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Control
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/whole/control.csv"), finish_only = TRUE, round = TRUE)
@@ -234,9 +316,11 @@ comparative_box_plots <- function() {
     g_control <- g_control + geom_boxplot(notch = TRUE)
     g_control <- g_control + labs(title = "Control", x = "", y = "")
     g_control <- g_control + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_control <- g_control + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_control <- g_control + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Fusion
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/whole/fusion.csv"), finish_only = TRUE, round = TRUE)
@@ -248,9 +332,11 @@ comparative_box_plots <- function() {
     g_fusion <- g_fusion + geom_boxplot(notch = TRUE)
     g_fusion <- g_fusion + labs(title = "Fusion", x = "", y = "")
     g_fusion <- g_fusion + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_fusion <- g_fusion + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_fusion <- g_fusion + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Localization
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/whole/localization.csv"), finish_only = TRUE, round = TRUE)
@@ -263,14 +349,16 @@ comparative_box_plots <- function() {
     g_localization <- g_localization + geom_boxplot(notch = TRUE)
     g_localization <- g_localization + labs(title = "Localization", x = "", y = "")
     g_localization <- g_localization + stat_summary(fun.y = mean, geom = "point", shape = 20, size = 6, color = "#ED5485", fill = "#ED5485")
-    g_localization <- g_localization + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(axis.title.x=element_blank(),
+    g_localization <- g_localization + scale_fill_Publication() + theme_Publication() + theme(legend.position = "none") + theme(
+        axis.title.x = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())
+        axis.ticks.x = element_blank()
+    )
 
     # Plot all CPU components
     if (1) {
         g <- ggarrange(g_recognition, g_planning, g_control, g_fusion, g_radar, g_localization, ncol = 3, nrow = 2)
-        my_plot(g, "CPU_box", width = 13, height = 7) 
+        my_plot(g, "CPU_box", width = 13, height = 7)
     }
 
     ######### Series plots for planning, trafficlight, detection and radar
@@ -291,7 +379,7 @@ comparative_box_plots <- function() {
     g_trafficlight <- g_trafficlight + geom_point(size = 3)
     g_trafficlight <- g_trafficlight + labs(title = "TrafficlightDetection (GPU-involved)", x = "", y = "")
     g_trafficlight <- g_trafficlight + scale_color_Publication() + theme_Publication() + theme(legend.position = "none")
-    
+
     # planning
     df_whole <- load_data(paste0("../data/dataset1/", dataset_str, "/whole/planning.csv"), finish_only = TRUE, round = TRUE)
     df_solo <- load_data(paste0("../data/dataset1/", dataset_str, "/solo/planning.csv"), finish_only = TRUE, round = TRUE)
@@ -359,7 +447,6 @@ align_with_lag <- function(df_whole, df_solo, lag = 0) {
 
 compute_stats <- function(df_whole, df_solo) {
     # First get diff and diff_portion
-
 }
 
 remove_outliers <- function(df) {
@@ -381,4 +468,5 @@ segment_df <- function(df, start = 1, len = 200) {
 
 # comparative_test_all()
 # comparative_test_single("../data/dataset1/6", "prediction")
-comparative_box_plots()
+# comparative_box_plots()
+box_plot_autoware()
